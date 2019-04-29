6449c9b8242b81e84b2a755dababbdba2860c1e2
macbook <hai.lemanh.1310@gmail.com>
2019-04-29 22:27:07 +0700

    update

--- footer.php
+++ footer.php
@@ -1,1 +1,55 @@
-<?php
+<footer id="footer" class="uk-background-bottom-right uk-background-norepeat" style="background-image: url(imgs/bg_f.png)">
+    <div class="uk-section-xsmall top">
+        <div class="uk-container uk-container-small uk-text-center">
+            *Sản phẩm này không phải là thuốc và không có tác dụng thay thế thuốc chữa bệnh <br>
+
+            Thời gian phát huy hiệu quả của sản phẩm tùy thuộc vào cơ địa mỗi người
+        </div>
+    </div>
+    <div class="uk-section-small center">
+        <div class="uk-container uk-container-small uk-text-center">
+            <div class="box1 uk-margin">
+                Đạt các chứng chỉ quan trọng nhất về dược phẩm và thực phẩm của Mỹ
+            </div>
+            <div class="uk-flex-center" uk-grid>
+                <?php
+                $arr = array(
+                    array(
+                        'txt' => 'FDA',
+                        'src' => 'imgs/chungchi/1.png',
+                    ),
+                    array(
+                        'txt' => 'GMP',
+                        'src' => 'imgs/chungchi/2.png',
+                    ),
+                    array(
+                        'txt' => 'KOSHER',
+                        'src' => 'imgs/chungchi/3.png',
+                    ),
+                    array(
+                        'txt' => 'HALAL',
+                        'src' => 'imgs/chungchi/4.png',
+                    ),
+                    array(
+                        'txt' => 'ISO 2000:2011',
+                        'src' => 'imgs/chungchi/5.png',
+                    ),
+                );
+                foreach ($arr as $key => $value) { ?>
+                <div class="uk-width-auto">
+                    <div class="box2">
+                        <div class="txt"><?= $value['txt'] ?></div>
+                        <div class="box_img">
+                            <img src="<?= $value['src'] ?>" alt="">
+                        </div>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+    <div class="bottom"></div>
+</footer>
+</section>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,229 @@
-<?php
+<?php include('header.php'); ?>
+<div>
+    <img class="uk-width-1-1" src="imgs/imgx.png" alt="">
+</div>
+<div class="block6 uk-background-cover uk-background-norepeat uk-position-relative" style="background-image: url(imgs/bg7.png)">
+    <div class="uk-position-center cogai">
+        <div class="uk-container uk-container-small">
+            <div class="uk-child-width-1-2 uk-grid-large" uk-grid>
+                <div>
+                    <img src="imgs/tangcan.png" alt="">
+                </div>
+                <div>
+                    <img src="imgs/thanhloc.png" alt="">
+                </div>
+                <div>
+                    <img src="imgs/anngon.png" alt="">
+                </div>
+                <div>
+                    <img src="imgs/dadep.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="uk-container uk-container-small uk-text-center">
+        <img class="uk-position-relative uk-position-z-index" src="imgs/cogai.png" alt="">
+    </div>
+</div>
+<div class="uk-container uk-container-small uk-position-relative uk-position-z-index uk-section uk-padding-remove-top uk-text-center">
+    <img src="imgs/img5.png" alt="">
+</div>
+<div class="block5 uk-position-relative">
+    <img class="uk-position-bottom-left uk-position-z-index" src="imgs/bg6.png" alt="">
+    <div class="uk-container uk-container-small uk-section-xsmall">
+        <h3>Công dụng tuyệt vời của tảo Spirulina</h3>
+    </div>
+</div>
+<div class="block3 block4 uk-background-center-right uk-flex-middle uk-flex uk-background-norepeat" style="background-image: url(imgs/bg5.png)">
+    <div class="uk-container uk-container-small">
+        <div class="uk-flex-left" uk-grid>
+            <div class="uk-width-2-3@m">
+                <p><span>Tảo Spirulina</span> là một loại thực vật tự nhiên tồn tại trên trái đất từ <span>3,6 tỷ</span> năm trước, được tiến sĩ người <span>Pháp - Clement</span> phát hiện vào những năm <span>1960</span> khi đến thăm hồ Tchad ở Trung Phi. Tới nay đã có hơn <span>300</span> công trình nghiên cứu, khẳng định những giá trị dinh dưỡng tuyệt vời của loại <span>siêu thực phẩm</span> này.</p>
+                <p><span>Tổ chức Y tế Thế Giới (WHO)</span> khẳng định: <span>“Tảo Spirulina</span> là thực phẩm bảo vệ sức khỏe tốt nhất của loài người trong thế kỉ 21”</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="block3 uk-background-center-left uk-flex-middle uk-flex uk-background-norepeat" style="background-image: url(imgs/bg4.png)">
+    <div class="uk-container uk-container-small">
+        <div class="uk-flex-right" uk-grid>
+            <div class="uk-width-2-3@m">
+                <p><span>Tảo Spirulina</span> chứa hơn <span>100</span> chất dinh dưỡng thiết yếu cho cơ thể: Đạm, các vitamin, chất khoáng, axit béo không no, chất xơ, GLA và các chất chống oxy hóa đa dạng như Phycocyanin, Chlorophyll (diệp lục), Beta – carotene, Polysaccaride,…</p>
+                <ul class="uk-list uk-list-bullet">
+                    <li>Vitamin và khoáng chất có trong <span>1gr tảo Spirulina = 1kg</span> rau củ quả.</li>
+                    <li>Lượng đạm gấp <span>5 lần</span> trong trứng, <span>3 lần</span> trong thịt bò.</li>
+                    <li>Hàm lượng chất chống oxy hóa gấp <span>700 lần</span> quả táo xanh.</li>
+                </ul>
+                <p>Từ năm <span>1984</span>, tổ chức <span>IIMSAM</span> của <span>Liên Hiệp Quốc</span> đã sử dụng <span>Tảo Spirulina</span> để chữa bệnh suy dinh dưỡng, giúp tăng cường cân nặng và sức khỏe cho hơn hai triệu trẻ em và người già trên thế giới.</p>
+                <p class="uk-text-right">(nguồn: www.iimsam.org)</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+    <div class="uk-container uk-container-small">
+        <img src="imgs/img4.png" alt="">
+    </div>
+</div>
+<div class="block2 uk-background-norepeat uk-background-cover" style="background-image: url(imgs/bg3.png)">
+    <div class="uk-section uk-visible@m"></div>
+    <div class="uk-section uk-visible@m"></div>
+    <div class="uk-section uk-visible@m"></div>
+    <div class="uk-container uk-container-small uk-section">
+        <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+            <div class="uk-width-auto@s">
+                <img class="img1" src="imgs/img3.png" alt="">
+            </div>
+            <div class="uk-width-expand@s">
+                <div class="uk-padding-small box8">
+                    <h3>TÁC ĐỘNG TĂNG CÂN</h3>
+                    <h4>của <span>TẢO MẶT TRỜI</span></h4>
+                    <hr>
+                    <p><span>Tảo Mặt Trời tăng cân</span> có thành phần <span>95% Tảo Spirulina</span> nguyên chất, giàu kẽm và bổ sung vitamin C tự nhiên từ trái <span>sơ ri hữu cơ</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="uk-section uk-visible@m"></div>
+</div>
+<div class="block1 uk-background-norepeat uk-section uk-background-center-left" style="background-image: url(imgs/bg2.png)">
+    <div class="uk-container uk-container-small">
+        <img class="uk-visible@m" src="imgs/img2.png" alt="">
+        <div class="uk-hidden@m">
+            <div class="uk-card card1 uk-grid-collapse uk-margin" uk-grid>
+                <div class="">
+                    <div class="uk-grid-small uk-flex-middle" uk-grid>
+                        <div class="uk-width-auto">
+                            <div class="box7 uk-border-circle uk-box-shadow-large">01</div>
+                        </div>
+                        <div class="uk-width-expand">
+                            <h3 class="title2">DINH DƯỠNG TOÀN DIỆN</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="uk-card-body uk-padding-small">
+                        <p>Cung cấp hơn <span>100</span> chất dinh dưỡng tự nhiên, thiết yếu. Hàm lượng <span>Đạm</span> toàn tính cao <span>(65%)</span> và <span>18/20</span> loại Axit amin cơ thể cần là nguyên liệu để tạo khung cho các cơ bắp, gân - điều kiện tiên quyết giúp tăng cân.</p>
+                        <p>Chất <span>GLA</span> kích thích tạo ra <span>Prostaglandin</span>, là các Hormone chủ đạo điều tiết mọi tế bào của cơ thể: tim, da, hệ tuần hoàn và hệ thống cơ. Thiếu Hormone này cơ thể sẽ không thể lên cân được.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-card card1 uk-grid-collapse uk-margin" uk-grid>
+                <div class="">
+                    <div class="uk-grid-small uk-flex-middle" uk-grid>
+                        <div class="uk-width-auto">
+                            <div class="box7 uk-border-circle uk-box-shadow-large">02</div>
+                        </div>
+                        <div class="uk-width-expand">
+                            <h3 class="title2">THANH LỌC,
+                                THẢI ĐỘC CƠ THỂ</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="uk-card-body uk-padding-small">
+                        <p>Tập hợp các chất <span>chống oxi hóa</span> mạnh nhất tự nhiên với hàm lượng cao giúp <span>thanh lọc cơ thể</span> cấp độ tế bào, giúp <span>tăng cường sức đề kháng</span>, sức khỏe tổng thể tạo đà giúp <span>tăng cân chắc khỏe</span>.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-card card1 uk-grid-collapse uk-margin" uk-grid>
+                <div class="">
+                    <div class="uk-grid-small uk-flex-middle" uk-grid>
+                        <div class="uk-width-auto">
+                            <div class="box7 uk-border-circle uk-box-shadow-large">03</div>
+                        </div>
+                        <div class="uk-width-expand">
+                            <h3 class="title2">KÍCH THÍCH
+                                ĂN NGON MIỆNG</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="uk-card-body uk-padding-small">
+                        <p>Tăng cảm giác <span>thèm ăn</span>, tăng cường quá trình tổng hợp <span>Protein</span>, phân chia tế bào, thúc đẩy sự phát triển hệ cơ ở người gầy nhờ tác dụng của khoáng chất <span>Kẽm</span> và các <span>Vitamin</span> tự nhiên.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-card card1 uk-grid-collapse uk-margin" uk-grid>
+                <div class="">
+                    <div class="uk-grid-small uk-flex-middle" uk-grid>
+                        <div class="uk-width-auto">
+                            <div class="box7 uk-border-circle uk-box-shadow-large">04</div>
+                        </div>
+                        <div class="uk-width-expand">
+                            <h3 class="title2">NGỦ NGON GIẤC</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div class="uk-card-body uk-padding-small">
+                        <p>Mang lại <span>giấc ngủ sâu, tự nhiên, giảm cảm giác mệt mỏi, stress</span> nhờ tác dụng của các <span>Vitamin nhóm B</span> (từ B1 – B12) và <span>2 Axit amin</span> (Tryptophan và Phenylalanin).</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-background-cover uk-background-norepeat" style="background-image: url(imgs/bg1.png)">
+    <div class="uk-container uk-container-small">
+        <img src="imgs/img1.png" alt="">
+    </div>
+    <div class="uk-margin uk-text-center">
+        <h3 class="title1">CÔNG NGHỆ ĐỘC ĐÁO CỦA TẢO MẶT TRỜI</h3>
+        <img src="imgs/traidat.png" alt="">
+    </div>
+    <div class="uk-text-center box6 uk-margin-medium">
+        Sản phẩm của hãng Earthrise Nutritionalls, hãng tảo lâu đời và lớn nhất trên thế giới
+    </div>
+    <div class="uk-container uk-container-small">
+        <div class="uk-child-width-1-2@m uk-grid-small" uk-grid>
+            <?php
+            $arr = array(
+                array(
+                    'txt' => 'Nuôi trồng hoàn toàn tự nhiên ở sa mạc Sonoran, California, đảm bảo các điều kiện khắt khe nhất',
+                    'src' => 'imgs/news1.png',
+                ),
+                array(
+                    'txt' => 'Quy trình thu hoạch hiện đại bằng công nghệ sấy hơi và ép lạnh, hoàn toàn tự động chỉ trong 15 phút, giữ nguyên vẹn các chất dinh dưỡng trong tảo',
+                    'src' => 'imgs/news2.png',
+                ),
+            );
+            foreach ($arr as $key => $value) { ?>
+            <div>
+                <div class="uk-cover-container box5">
+                    <img src="<?= $value['src'] ?>" alt="" uk-cover>
+                    <canvas width="483" height="325"></canvas>
+                    <div class="uk-padding-small uk-position-bottom">
+                        <?= $value['txt'] ?>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+<div class="uk-section-small">
+    <div class="uk-container uk-container-small">
+        <div class="uk-grid-collapse uk-flex-middle uk-flex-center" uk-grid>
+            <div class="uk-position-relative box3">
+                <img class="uk-position-center-left-out taobien uk-visible@s" src="imgs/taobien.png" alt="">
+                <img class="lothuoc" src="imgs/3lothuoc.png" alt="">
+            </div>
+            <div class="uk-position-relative">
+                <img src="imgs/khungtxt.png" alt="">
+                <div class="uk-padding uk-position-center box4">
+                    Trải qua <b>66</b> lần kiểm tra về
+                    <span>chất lượng</span> và <span>an toàn</span>
+                    trước khi được đóng gói
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-position-relative">
+    <img class="uk-position-center" src="imgs/comy.png" alt="">
+    <div class="uk-container uk-container-small">
+        <img class="uk-position-relative uk-position-z-index" src="imgs/taomattroi.png" alt="">
+    </div>
+</div>
+<?php include('footer.php'); ?>
